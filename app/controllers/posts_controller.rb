60439596b005bc046e03619781c9a34e0402a68f
tsukisawa-yuya <mh21sus@gmail.com>
2018-08-26 09:42:00 +0900

    show new create edit update作成

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,39 @@
+class PostsController < ApplicationController
+  def index
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.save
+    redirect_to @post
+  end
+
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    @post.update(post_params)
+    redirect_to @post
+  end
+
+  def destroy
+  end
+
+private
+
+ def post_params
+  params. require(:post).permit(:title, :body, :category)
+ end 
+
+end
