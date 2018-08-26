77185920d012db7b6df5923e6a609de5d3f54617
tsukisawa-yuya <mh21sus@gmail.com>
2018-08-26 11:07:44 +0900

    ブログ完成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,18 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
+  
+  
   def index
+    @q = Post.order(created_at: :desc).ransack(params[:q])
+    @posts = @q.result.page(params[:page]).per(2)
+
+    @new_posts = Post.find_newest_article
+    
+    @author = Author.first
   end
 
   def show
-    @post = Post.find(params[:id])
+    
   end
 
   def new
@@ -18,19 +27,24 @@ class PostsController < ApplicationController
 
 
   def edit
-    @post = Post.find(params[:id])
+   
   end
 
   def update
-    @post = Post.find(params[:id])
     @post.update(post_params)
     redirect_to @post
   end
 
   def destroy
+    @post.destroy
+    redirect_to posts_path
   end
 
 private
+
+def set_post
+  @post = Post.find(params[:id])
+end  
 
  def post_params
   params. require(:post).permit(:title, :body, :category)
